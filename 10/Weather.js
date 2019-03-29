92917d70948106982ae21c33493bdea7d78bf795
Denis Sinkevich <sinkevich_denis@inbox.ru>
2019-03-29 22:47:48 +0300

    feat(lesson-10): add first variant weather window

---
+++ 10/Weather.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const SHOW_PLACE = document.querySelector('.weather_place');
+const TITLE_PLACE = document.querySelector('.weather_title');
+const KEY_ID = '595e9af527f3090cb8632eb0e4960744';
+const URL = 'http://api.openweathermap.org/data/2.5/forecast';
+
+const SEARCH_OBJ = {
+	name: 'Гродно',
+	param: [
+		'lat=53.68',
+		'lon=23.82',
+		'cnt=10',
+		'lang=ru',
+		'units=metric',
+		`APPID=${KEY_ID}`
+	]
+};
+
+function createTitle(path, obj){
+	path.innerHTML =  `Погода в ${SEARCH_OBJ.name} <br /> ${obj.dt_txt}`
+}
+
+function loadData(url, obj) {
+	const xhr = new XMLHttpRequest();
+	const path = url + '?' + obj.param.join('&');
+
+	xhr.open('GET', path, true);
+	xhr.send();
+
+	xhr.onload = function() {
+		renderData(JSON.parse(xhr.responseText), SHOW_PLACE);
+	};
+}
+
+function renderData(object, div) {
+	let obj =object.list[5];
+	createTitle(TITLE_PLACE, obj);
+
+	div.innerHTML  = `
+		<table class="weather_table">
+			<tr><td>${obj.weather[0].description}</td></tr>
+			<tr>
+				<td>Температура </td><td>${obj.main.temp} C</td>
+			</tr>
+			<tr>
+				<td>Влажность </td><td>${obj.main.humidity} %</td>
+			</tr>
+			<tr>
+				<td>Ветер </td><td>${obj.wind.speed} м/с</td>
+			</tr>
+		</table>`;
+}
+
+loadData(URL, SEARCH_OBJ);
+
+
